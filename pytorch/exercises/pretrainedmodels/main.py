47883b6c65b8d05a0d1c5737f1552df6476ded34
Ye Wang <905317742@qq.com>
2020-10-28 17:32:24 +0800

    格式化代码

--- pytorch/exercises/pretrainedmodels/main.py
+++ pytorch/exercises/pretrainedmodels/main.py
@@ -34,11 +34,11 @@ tf_img = utils.TransformImage(model)
 path_img = 'data/cat.jpg'
 
 input_img = load_img(path_img)
-input_tensor = tf_img(input_img)         # 3x400x225 -> 3x299x299 size may differ
-input_tensor = input_tensor.unsqueeze(0) # 3x299x299 -> 1x3x299x299
-input = torch.autograd.Variable(input_tensor,requires_grad=False)
+input_tensor = tf_img(input_img)  # 3x400x225 -> 3x299x299 size may differ
+input_tensor = input_tensor.unsqueeze(0)  # 3x299x299 -> 1x3x299x299
+input = torch.autograd.Variable(input_tensor, requires_grad=False)
 
-output_logits = model(input) # 1x1000
+output_logits = model(input)  # 1x1000
 print(output_logits)
 # output_features = model.features(input) # 1x14x14x2048 size may differ
 # output_logits = model.logits(output_features) # 1x1000
